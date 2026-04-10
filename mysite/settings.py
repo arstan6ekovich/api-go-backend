9fcc335c1570105cfaaeeb4b91ebdf71f19d3cb7
arstan6ekovich <zomadujsonov@gmail.com>
2026-04-10 22:51:57 +0600

    mar4ik

--- mysite/settings.py
+++ mysite/settings.py
@@ -65,10 +65,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-}
+DATABASE_URL = os.getenv('DATABASE_URL')
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
